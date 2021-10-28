07caaca0571730b6a9493e9de136cb04c3f6ff34
jakezhang <24jzhang@athenian.org>
2021-10-28 16:54:34 -0700

    ok

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,15 @@
-public class NinthGrader { // SOME CODE NEEDED IN THIS LINE
-    // YOUR CODE HERE
+public class NinthGrader extends Student{ // SOME CODE NEEDED IN THIS LINE
+    public NinthGrader(String a,String b,int c){
+        super(a,b,c);
+    }
+    public NinthGrader() {
+        super("Jazz", "Bruh",2025);
+    }
+    public void doSomething() {
+        System.out.println("I am preparing for a Harkness discussion!");
+    }
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", 2025, (Ninth Grader)";
+        return str;
+    }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,26 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    public Person(String a, String b){
+        firstName=a;
+        lastName=b;
+    }
+    public void doSomething(){
+        System.out.println("I am watching Netflix.");
+    }
+    public String toString() {
+        return (firstName +" " +lastName);
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public void setFirstName(String x){
+        firstName=x;
+    }
+    public void setLastName(String x){
+        lastName=x;
+    }
 }
